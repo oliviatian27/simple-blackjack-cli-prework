5cf091cb38936545c82fa878948a33f85410f8a7
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-22 00:50:48 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,9 +32,9 @@ end
 
 def initial_round
   # code #initial_round here
-  
-  display_card_total(deal_card+deal_card)
-  return deal_card+deal_card
+  card_total=deal_card+deal_card
+  display_card_total(card_total)
+  return card_total
 end
 
 def hit?(n)
